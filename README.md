ac190efcfa3e304e8cf068a9114ccf0e7c5d94c0
Curder <q.curder@gmail.com>
2020-10-31 16:36:49 +0800

    gin加载静态资源

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - [自定义模版标识符](/demo8/README.md)
 - [gin模版渲染](/demo9/README.md)
 - [gin自定义模版函数](/demo10/README.md)
+- [gin加载静态资源](/demo11/README.md)
---
+++ demo11/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	var (
+		c   *gin.Engine
+		err error
+	)
+
+	c = gin.Default()
+
+	c.Static("/public", "./public") // 加载静态资源，比如css,js,images,fonts等
+
+	c.LoadHTMLGlob("./views/*.tpl") // 加载模版
+
+
+
+	c.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tpl", gin.H{
+			"name": "curder",
+		})
+	})
+
+	if err = c.Run(":8080"); err != nil {
+		fmt.Printf("Server start failed,error: %s", err.Error())
+		return
+	}
+}
